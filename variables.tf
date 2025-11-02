db2472da5e5f7492c5dcda99b45ae8fb2dc5343e
somayya <somayya.adusumalli98@gmail.com>
2025-11-02 10:18:16 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "sg_description" {
+  type = string
+  description = "value"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "sg_tags" {
+  type = map
+  default = {}
+}
